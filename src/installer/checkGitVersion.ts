d5ace9e752d8704a908b27a57d3a142cdb50df52
Eduard Malakhov <eduard@atweel.com>
2020-03-23 02:21:16 +0000

    Add debug trace for git version inside checkGitVersion

--- src/installer/checkGitVersion.ts
+++ src/installer/checkGitVersion.ts
@@ -1,6 +1,7 @@
 import cp = require('child_process')
 import findVersions from 'find-versions'
 import compareVersions from 'compare-versions'
+import { debug } from '../debug'
 
 export function checkGitVersion(): void {
   const { status, stderr, stdout } = cp.spawnSync('git', ['--version'])
@@ -10,6 +11,8 @@ export function checkGitVersion(): void {
   }
 
   const [version] = findVersions(stdout.toString())
+
+  debug(`Git version is ${version}`)
 
   if (compareVersions(version, '2.13.0') === -1) {
     throw new Error(`Husky requires Git >=2.13.0. Got v${version}.`)
